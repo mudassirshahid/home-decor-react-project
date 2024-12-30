65e392fe14bea27e144835b6055b3b9931e71c83
Mudassir <mudassirshahid84@gmail.com>
2024-12-30 16:25:54 +0500

    add custom carousel

---
+++ src/components/customCarousel.jsx
@@ -0,0 +1,46 @@
+import { Carousel as AntCarousel } from "antd"; // Rename the import
+import bannerImg from "../../src/assets/images/2.png";
+import bannerImgNext from "../../src/assets/images/5.png";
+
+
+function CustomCarousel() {
+  // Rename your function
+  const contentStyle = {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+
+  return (
+    <>
+    <AntCarousel autoplay autoplaySpeed={2500} draggable="true" effect="fade" infinite="true">
+      <div className="px-10  h-auto">
+        <div className="flex sm:flex-row flex-col justify-center items-center mx-auto">
+          <div className="sm:text-left text-center sm:w-[50%] w-full sm:px-0 px-5 lg:mt-0 mt-16">
+            <h3 className="2xl:text-8xl xl:text-7xl lg:text-5xl text-4xl font-light uppercase ">
+              Your Products Are Great.
+            </h3>
+            <button className="bg-slate-800 text-slate-50 text-base font-normal sm:my-10 my-4 sm:px-12 px-7 py-4 uppercase">Shop Product</button>
+          </div>
+          <img className="lg:w-[40%] md:w-[50%] w-[60%]" src={bannerImg} alt="banner Iamge" />
+        </div>
+      </div>
+      <div className="bg-slate-200 px-10  h-auto">
+        <div className="flex sm:flex-row flex-col justify-center items-center mx-auto">
+          <div className="sm:text-left text-center sm:w-[50%] w-full sm:px-0 px-5 lg:mt-0 mt-16">
+            <h3 className="2xl:text-8xl xl:text-7xl lg:text-5xl text-4xl font-light uppercase ">
+              Your Products Are Great.
+            </h3>
+            <button className="bg-slate-800 text-slate-50 text-base font-normal sm:my-10 my-4 sm:px-12 px-7 py-4 uppercase">Shop Product</button>
+          </div>
+          <img className="lg:w-[40%] md:w-[50%] w-[60%]" src={bannerImgNext} alt="banner Iamge" />
+        </div>
+      </div>
+    </AntCarousel>
+    </>
+  );
+}
+
+export default CustomCarousel; // Export your renamed component
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { products } from '../products'
 import ProductCart from '../components/productCart'
+import CustomCarousel from '../components/customCarousel'
 const Home = () => {
   return (
     <div className='p-5'>
-        <h1 className='text-3xl my-5 text-center'>List Products</h1>
+      <CustomCarousel />
+        <h1 id='products' className='text-3xl my-5 text-center'>List Products</h1>
         <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
             {products.map((product, key) => 
                 <ProductCart key={key} data={product}/>
